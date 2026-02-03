d714386bdd9d45bb4187b71b85c520f07dfe2530
bmonroy55 <bmonroy55@yahoo.com>
2026-02-03 12:31:14 -0800

    Update config.js

--- config.js
+++ config.js
@@ -23,35 +23,35 @@ const CONFIG = {
     questions: {
         first: {
             text: "Do you like me? 😁",                                    // First interaction
-            yesBtn: "Yes",                                             // Text for "Yes" button
-            noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            yesBtn: "Yes (Wrong)",                                             // Text for "Yes" button
+            noBtn: "No (Wrong)",                                               // Text for "No" button
+            secretAnswer: "I don't like you, I love you. 🖤"           // Secret hover message
         },
         second: {
-            text: "How much do you love me?",                          // For the love meter
+            text: "How much do you love me? 😁",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
-            nextBtn: "Next ❤️"                                         // Text for the next button
+            nextBtn: "Next 🖤"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
-            yesBtn: "Yes!",                                             // Text for "Yes" button
-            noBtn: "No"                                                 // Text for "No" button
+            text: "Will you be my Valentine on February 14th, 2026? 🌹...", // The big question!
+            yesBtn: "Of course 🖤",                                             // Text for "Yes" button
+            noBtn: "No 💔"                                                 // Text for "No" button
         }
     },
 
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "I love you more 🌹🖤😁",  // Shows when they go past 5000%
+        high: "Getting there... 😁",              // Shows when they go past 1000%
+        normal: "More 😡😡"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: "I love you forever and ever babe.",
+        message: "Now come give me a big smooch, MWAHHHHHHH",
+        emojis: "🖤💜🧸🐻🌹🖤💜🧸🐻🌹"  // These will bounce around
     },
 
     // Color scheme for the website
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dsnqni1kc/video/upload/v1770094995/Tonight_Tonight_ttckph.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
